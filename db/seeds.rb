8777151e2dfa612747df69fa8a44d6ef3b644b4e
Miucha Salge <miucha.salge@gmail.com>
2019-05-23 16:39:35 -0300

    Routes, controllers and views created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts 'Cleaning database...'
+Ingredient.destroy_all
+
+puts 'Creating ingredients...'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+list_serialized = open(url).read
+list = JSON.parse(list_serialized)
+ingredients = list['drinks']
+
+ingredients.each do |ingredient|
+  ingredient = ingredient['strIngredient1'].to_s
+  Ingredient.create(name: ingredient)
+end
+
+puts 'Ingredients created'
